1abedee132118e594b6b85fb3b19396404e92101
Yama1109 <yamachiyan119@gmail.com>
2024-04-24 16:27:12 +0900

    プルの練習

--- front/src/components/UploadButton.vue
+++ front/src/components/UploadButton.vue
@@ -1,29 +1,35 @@
 <template>
-    <button @click="handleClick">
-        Click me
-    </button>
+    <button @click="handleClick" class="cool-button">Click me</button>
 </template>
   
 <script>
-    export default {
-        methods: {
-            handleClick() {
-            // ボタンがクリックされたときの処理を記述する
-            console.log('Button clicked!');
-            }
-        }
+  export default {
+    methods: {
+      handleClick() {
+        console.log('Button clicked!');
+      }
     }
+  }
 </script>
   
 <style>
-  /* ボタンのスタイルを記述する */
-  button {
-    background-color: yellowgreen;
-    color: purple;
-    padding: 10px 20px;
+  .cool-button {
+    background-color: green;
     border: none;
-    border-radius: 10px;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    transition-duration: 0.4s;
     cursor: pointer;
+    border-radius: 8px;
+  }
+  
+  .cool-button:hover {
+    background-color: #45a049; /* Darker Green */
   }
 </style>
   
