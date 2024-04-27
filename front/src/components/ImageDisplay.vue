b294ad541d95e02510b4189e5798d6eadfcba017
taro22 <ryutaro1117@icloud.com>
2024-04-27 20:59:18 +0900

    非同期でサーバから画像を取ってくるようなコンポーネントを作成

--- front/src/components/ImageDisplay.vue
+++ front/src/components/ImageDisplay.vue
@@ -1,15 +1,27 @@
 <template>
-    <button>
-        <!-- 【sあの】ボタンのデザインはここ -->
-    </button>
+    <img :src="image" alt="">
 </template>
 
 <script>
-export default  {
-    name: 'ImageDisplay',
-}
+import { ref, defineComponent, onMounted } from 'vue'
+
+export default  defineComponent({
+    setup() {
+        // refでリアクティブにして値を更新されると反映
+        let image = ref('')
+    
+        onMounted(async () => {
+            // サーバーから非同期で画像URLを取得
+            image.value = await getImage()
+        })
+    
+        return {
+            image
+        }
+    }
+})
 </script>
 
 <style>
-    /*【sあの】デザインはここ */
+
 </style>
