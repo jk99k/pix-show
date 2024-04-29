5818b667f371d85e90f0cf2968f9dc1d85090fe7
taro22 <ryutaro1117@icloud.com>
2024-04-29 19:04:57 +0900

    半動的に画像を表示可能にした。

--- front/src/components/ImageDisplay.vue
+++ front/src/components/ImageDisplay.vue
@@ -1,5 +1,8 @@
 <template>
-    <img :src="image" alt="">
+    <div>
+      <!-- 画像を表示する -->
+      <img :src="image" alt="Server Image">
+    </div>
   </template>
   
   <script setup>
@@ -9,8 +12,28 @@
   
   // サーバーから画像を取得する関数
   const fetchImage = () => {
-    const imageUrl = 'http://127.0.0.1:8000/uploads/082_Original.jpg' // 画像のURLを指定
-    image.value = imageUrl // 画像のURLをセット
+    try {
+      // サーバーから画像のURLを取得するためのリクエストを送信
+      fetch('http://127.0.0.1:8000')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Failed to fetch image');
+          }
+          // レスポンスから JSON データを取得
+          return response.json();
+        })
+        .then(data => {
+            console.log(data)
+          // JSON データから画像の URL を取得してセット
+          const imageUrl = data.files[1];
+          image.value = `http://localhost:8000/uploads/${imageUrl}`;
+        })
+        .catch(error => {
+          console.error('Error fetching image:', error);
+        });
+    } catch (error) {
+      console.error('Error fetching image:', error);
+    }
   }
   
   // コンポーネントがマウントされたときに画像を取得する
