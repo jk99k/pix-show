4885d59a6c81e49d82fb141ab87e66bcaea0493e
taro22 <ryutaro1117@icloud.com>
2024-04-29 18:35:15 +0900

    コンポーネントがマウントされたときに画像を取得し、表示できるようにした

--- front/src/components/ImageDisplay.vue
+++ front/src/components/ImageDisplay.vue
@@ -1,27 +1,21 @@
 <template>
     <img :src="image" alt="">
-</template>
-
-<script>
-import { ref, defineComponent, onMounted } from 'vue'
-
-export default  defineComponent({
-    setup() {
-        // refでリアクティブにして値を更新されると反映
-        let image = ref('')
-    
-        onMounted(async () => {
-            // サーバーから非同期で画像URLを取得
-            image.value = await getImage()
-        })
-    
-        return {
-            image
-        }
-    }
-})
-</script>
-
-<style>
-
-</style>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue'
+  
+  const image = ref('') // 画像のURLを格納するリファレンス
+  
+  // サーバーから画像を取得する関数
+  const fetchImage = () => {
+    const imageUrl = 'http://127.0.0.1:8000/uploads/082_Original.jpg' // 画像のURLを指定
+    image.value = imageUrl // 画像のURLをセット
+  }
+  
+  // コンポーネントがマウントされたときに画像を取得する
+  onMounted(() => {
+    fetchImage();
+  })
+  </script>
+  
