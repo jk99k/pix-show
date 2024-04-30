eac0fb86f8cb32797e8cdd0d3c6509c3c02b2912
popohato <64397377+popohato@users.noreply.github.com>
2024-04-30 21:53:19 +0900

    ファイル数の制限、ファイル名のUUID化

--- back/main.py
+++ back/main.py
@@ -1,5 +1,7 @@
 import os
 import shutil
+from pathlib import Path
+from uuid import uuid4
 
 from fastapi import FastAPI, File, HTTPException, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
@@ -27,7 +29,22 @@ def save_uploaded_file(upload_dir: str, upload_file: UploadFile) -> None:
     try:
         # ファイルを保存する
         if upload_file.filename is not None:
-            with open(os.path.join(upload_dir, upload_file.filename), "wb") as buffer:
+            # ファイル名をUUIDに変更する
+            file_uuid = str(uuid4())
+            file_extention = Path(upload_file.filename).suffix
+            new_filename = f"{file_uuid}{file_extention}"
+            
+            # 保存先ディレクトリが存在しない場合は作成する
+            os.makedirs(upload_dir, exist_ok=True)
+            
+            # 保存先ディレクトリ内のファイル数を確認し、100を超えている場合は一番古いファイルを削除する
+            files_in_dir = os.listdir(upload_dir)
+            if len(files_in_dir) >= 100:
+                oldest_file = min(files_in_dir, key=lambda f: os.path.getctime(os.path.join(upload_dir, f)))
+                os.remove(os.path.join(upload_dir, oldest_file))
+
+            # ファイルを保存する
+            with open(os.path.join(upload_dir, new_filename), "wb") as buffer:
                 shutil.copyfileobj(fsrc=upload_file.file, fdst=buffer)
     finally:
         # ファイルを閉じる
